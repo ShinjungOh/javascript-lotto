cee92489422b68e3ed5ab6c81e420aab41de0ecf
ShinjungOh <tlswjd9260@gmail.com>
2024-12-11 09:18:29 +0900

    test: Lotto 클래스 유효성 검사 추가 및 테스트 작성

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -10,10 +10,29 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    this.#isDuplicate(numbers);
+    this.#checkLottoNumberRange(numbers);
   }
 
   get lotto() {
     return this.#numbers;
+  }
+
+  #isDuplicate(numbers) {
+    numbers.forEach((number) => {
+      if (numbers.includes(number)) {
+        throw new Error('[ERROR] 중복 번호는 입력할 수 없습니다.');
+      }
+    });
+  }
+
+  #checkLottoNumberRange(numbers) {
+    numbers.forEach((number) => {
+      if (number < 1 || number > 45) {
+        throw new Error('[ERROR] 로또 번호는 1부터 45 사이의 숫자입니다.');
+      }
+    });
   }
 }
 
